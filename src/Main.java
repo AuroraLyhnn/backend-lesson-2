6048a5197e2746e57696639c2ced2531717dac5b
AuroraLyhnn <sisserahvds@gmail.com>
2025-03-19 20:24:20 +0100

    hoofdopdracht en deel van if/else bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,100 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int number;
+        int input1;
+        int input2;
+        String name;
+
+        hello();
+
+        System.out.println("Type in a number");
+        number = sc.nextInt();
+        positiveOrNegative(number);
+
+        System.out.println("Type in another number");
+        number = sc.nextInt();
+        sc.nextLine();
+        positiveOrZeroOrNegative(number);
+
+        System.out.println("Tell me, who is this drink for?");
+        name = sc.nextLine();
+        bartender(name);
+
+        System.out.println("Give me a number");
+        input1 = sc.nextInt();
+        System.out.println("What number do you want to add");
+        input2 = sc.nextInt();
+        sum(input1, input2);
+
+        evenOddChecker(sc);
+        bonus.grader(sc);
+
+
     }
+
+    public static void hello() {
+        System.out.println("Hello, world!");
+    }
+
+    public static void positiveOrNegative(int number) {
+        if (number > 0) {
+            System.out.println("This number is positive!");
+        } else {
+            System.out.println("This number is negative!");
+        }
+    }
+
+    public static void positiveOrZeroOrNegative(int number) {
+        if (number > 0) {
+            System.out.println("This number is positive!");
+        } else if (number == 0) {
+            System.out.println("This number is zero!");
+        } else {
+            System.out.println("This number is - negative!");
+        }
+    }
+
+    public static void bartender(String name) {
+        switch (name) {
+            case "Jeroen":
+                System.out.println(name + " wants a whiskey on the rocks");
+                break;
+            case "Henk":
+                System.out.println(name + " wants a Bacardi cola");
+                break;
+            case "Mitch":
+                System.out.println(name + " wants a Beer");
+                break;
+            case "Mark":
+                System.out.println(name + " wants an Gin and Tonic");
+                break;
+            case "Jack Sparrow":
+                System.out.println(name + " wants a bottle of rum");
+                break;
+            default:
+                System.out.println("I Dont know your drink yet, but please do tell");
+        }
+
+    }
+    public static void sum(int input1, int input2) {
+        int sum = input1 + input2;
+        System.out.println(input1 + " summed by " + input2 + " = " + sum);
+    }
+
+    public static void evenOddChecker (Scanner sc){
+        int input3 = sc.nextInt();
+        sc.nextLine();
+        if (input3 % 2 == 0) {
+            System.out.println("Het getal is even");
+        } else {
+            System.out.println("Het getal is oneven");
+        }
+    }
+
+
+
 }
--- src/bonus.java
+++ src/bonus.java
@@ -1,4 +1,114 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class bonus {
+
+    public static void evenOddChecker(Scanner sc) {
+        System.out.println("Voer een getal in");
+        int number = sc.nextInt();
+        sc.nextLine();
+        int result = number % 2;
+        if (result > 0){
+            System.out.println("Het getal is even");
+        } else {
+            System.out.println("Het getal is oneven");
+        }
+    }
+
+
+    public static void grader(Scanner sc) {
+        System.out.println("Wat is je cijfer?");
+        int grade = sc.nextInt();
+        sc.nextLine();
+        switch (grade) {
+            case 1:
+            case 2:
+                System.out.println("Je hebt een F gehaald, dat is een onvoldoende");
+            case 3:
+            case 4:
+                System.out.println("Je hebt een D gehaald, dit is een onvoldoende");
+            case 5:
+            case 6:
+                System.out.println("Je hebt een C gehaald, dit is een voldoende");
+            case 7:
+            case 8:
+                System.out.println("Je hebt een B gehaald, nette voldoende!");
+            case 9:
+            case 10:
+                System.out.println("Je hebt een A, wauw");
+            default:
+                System.out.println("Dit cijfer klopt niet");
+        }
+    }
+
+    public static void highestNumber(Scanner sc) {
+        System.out.println("Voer een getal in");
+        int numberOne = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Voer nog een getal in");
+        int numberTwo = sc.nextInt();
+        sc.nextLine();
+        if (numberOne > numberTwo) {
+            System.out.println(numberOne + "is het hoogste getal");
+        } else if (numberOne < numberTwo) {
+            System.out.println(numberTwo + "is het hoogste getal");
+        } else {
+            System.out.println("De getallen zijn even hoog");
+        }
+    }
+
+    public static void positiveNegativeZero(Scanner sc) {
+        int number = sc.nextInt();
+        sc.nextLine();
+        if (number > 0) {
+            System.out.println("Dit getal is positief");
+        } else if (number == 0) {
+            System.out.println("Dit getal is nul!");
+        } else {
+            System.out.println("Dit getal is negatief");
+        }
+    }
+
+    public static void age(Scanner sc) {
+        int age = sc.nextInt();
+        sc.nextLine();
+        if (age >= 70) {
+            System.out.println("Je bent een senior");
+        } else if (age > 18) {
+            System.out.println("Je bent een volwassene");
+        } else {
+            System.out.println("Je bent een kind");
+        }
+    }
+
+    public static void textLength(Scanner sc) {
+        String text = sc.nextLine();
+        if (text.length() < 10) {
+            System.out.println("kort");
+        } else if (text.length() < 20) {
+            System.out.println("middellang");
+        } else {
+            System.out.println("lang");
+        }
+    }
+
+    public static void leapYearOrNot(Scanner sc) {
+        int year = sc.nextInt();
+        sc.nextLine();
+        if (year % 4 != 0) {
+            System.out.println("Het is geen schrikkeljaar");
+        } else {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("Het is een schrikkeljaar!");
+                } else {
+                    System.out.println("Het is geen schrikkeljaar");
+                }
+            } else {
+                System.out.println("Het is een schrikkeljaar!");
+            }
+        }
+
+    }
+
+
 }
